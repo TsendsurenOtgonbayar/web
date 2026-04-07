e787d8a290bac78529229cda92b0fea570c43ec5
TsendsurenOtgonbayar <24B1NUM0377@stud.num.edu.mn>
2026-04-07 10:16:44 +0800

    js turshilt

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,5 @@
+import { delElm,CountElements,addStarColor } from "./utils.js";
+
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Main JS ажиллаж байна...");
 
@@ -34,4 +36,44 @@ document.addEventListener("DOMContentLoaded", () => {
         el.style.transition = "all 0.6s ease-out";
         observer.observe(el);
     });
+    // comment bichih hesgiih arilgalaa
+    const makeComment = document.getElementById("makeComments");
+    makeComment.style.display="none";
 });
+document.getElementById("CommentButton").addEventListener("click",()=>{
+    const makeComment = document.getElementById("makeComments");
+    const commentSection=document.getElementById("commentRow");
+    makeComment.style.display="flex";
+    addStarColor("#UnelgeeFiveStar svg");
+    makeComment.querySelector("#sentButton").addEventListener("click",()=>{
+        const textArea =makeComment.querySelector("#commenText");
+        const text=textArea.value;  
+        if(text.length<5) return 0;
+        //ene hesegt od bbutsadag function bichne
+        const newArticle= document.createElement("article");
+        newArticle.classList.add("commentKart");
+        const fiveStar= document.createElement("div");
+        fiveStar.id="UnelgeeFiveStar";
+        fiveStar.innerHTML=`<svg data-value="1" viewBox="0 0 24 24"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/></svg>
+            <svg data-value="2" viewBox="0 0 24 24"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/></svg>
+            <svg data-value="3" viewBox="0 0 24 24"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/></svg>
+            <svg data-value="4" viewBox="0 0 24 24"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/></svg>
+            <svg data-value="5" viewBox="0 0 24 24"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/></svg>`;
+        const commentText=document.createElement("p");
+        commentText.classList.add("commentText");
+        commentText.textContent=text;
+        const userName=document.createElement("h3");
+        userName.textContent="turshilt";
+        const petName=document.createElement("p");
+        petName.textContent="amitan";
+        newArticle.appendChild(fiveStar);
+        newArticle.appendChild(commentText);
+        newArticle.appendChild(userName);
+        newArticle.appendChild(petName);
+        commentSection.appendChild(newArticle);
+        delElm("commentRow","article");
+        textArea.value="";  
+        makeComment.style.display="none";
+    });
+}
+);
