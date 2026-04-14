4c84cbf50346af3b6122bc74424ceedf455987a6
TsendsurenOtgonbayar <24B1NUM0377@stud.num.edu.mn>
2026-04-14 22:49:59 +0800

    class nemj ehelle

---
+++ assets/js/classes/appointment.js
@@ -0,0 +1,12 @@
+export class Appointment {
+  constructor(userId, petId, service, appointmentDate) {
+    this.id = Date.now();
+    this.userId = userId;
+    this.petId = petId;
+    this.service = service;
+    this.appointmentDate = appointmentDate;
+    this.status = "Хүлээгдэж байна";
+    this.notes = "";
+    this.createdAt = new Date().toISOString();
+  }
+}
