cf7f5aaa2bc6905b5d7ef2604c9c72f0a5b32725
TsendsurenOtgonbayar <24B1NUM0377@stud.num.edu.mn>
2026-05-21 13:09:40 +0800

    imp .js

--- js/pages/login/renderLoginPage.js
+++ js/pages/login/renderLoginPage.js
@@ -1,7 +1,7 @@
-import renderLoginForm from "./loginSection";
-import renderRegisterForm from "./registerSection";
-import renderSelect from "./selectSectionRender";
-import cssChanger from "../../components/pagesCSS";
+import renderLoginForm from "./loginSection.js";
+import renderRegisterForm from "./registerSection.js";
+import renderSelect from "./selectSectionRender.js";
+import cssChanger from "../../components/pagesCSS.js";
 
 export default function renderLoginPage(){
     cssChanger(login);
